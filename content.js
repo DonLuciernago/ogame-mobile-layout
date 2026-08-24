fce2b53d3f34d7662c8ea4171a7336980bbe6f1b
DonLuciernago <pablitomonti@gmail.com>
2026-08-24 11:11:07 +0200

    Use content script only to request native zoom

--- content.js
+++ content.js
@@ -1,14 +1,1 @@
-(() => {
-  'use strict';
-
-  document.documentElement.classList.add('ogame-mobile-layout');
-
-  // Move the original OGame menu once. No cloning: existing links/listeners stay intact.
-  const pageContent = document.querySelector('#pageContent');
-  const menuTable = document.querySelector('#menuTable');
-  const middle = document.querySelector('#middle');
-
-  if (pageContent && menuTable && middle && menuTable.parentElement !== pageContent) {
-    pageContent.insertBefore(menuTable, middle);
-  }
-})();
+chrome.runtime.sendMessage({ type: 'APPLY_OGAME_ZOOM' });
