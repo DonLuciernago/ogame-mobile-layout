ded6f84507a365749ebd97d2a02199a4f00ceb89
DonLuciernago <pablitomonti@gmail.com>
2026-08-24 11:10:51 +0200

    Add native browser zoom handler

---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message?.type !== 'APPLY_OGAME_ZOOM') return;
+  if (!sender.tab?.id) return;
+
+  chrome.tabs.setZoom(sender.tab.id, 3.0);
+});
