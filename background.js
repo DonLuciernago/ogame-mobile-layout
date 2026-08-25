766f1d6a2075801dca8d7fb546b3056f2c340ca6
DonLuciernago <pablitomonti@gmail.com>
2026-08-25 10:18:46 +0200

    Increase native OGame zoom to 320 percent

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ chrome.runtime.onMessage.addListener((message, sender) => {
   if (message?.type !== 'APPLY_OGAME_ZOOM') return;
   if (!sender.tab?.id) return;
 
-  chrome.tabs.setZoom(sender.tab.id, 3.0);
+  chrome.tabs.setZoom(sender.tab.id, 3.2);
 });
