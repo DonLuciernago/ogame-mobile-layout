547e56bde88a24ff4fe8c53cabe2ea0c790e56c4
DonLuciernago <pablitomonti@gmail.com>
2026-08-25 10:21:20 +0200

    Remove viewport zoom lock

--- content.js
+++ content.js
@@ -1,14 +1,1 @@
 chrome.runtime.sendMessage({ type: 'APPLY_OGAME_ZOOM' });
-
-(() => {
-  const content = 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, interactive-widget=resizes-content';
-  let viewport = document.querySelector('meta[name="viewport"]');
-
-  if (!viewport) {
-    viewport = document.createElement('meta');
-    viewport.name = 'viewport';
-    document.head.appendChild(viewport);
-  }
-
-  viewport.setAttribute('content', content);
-})();
